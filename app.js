5b0ff40fbaa5608b3fff67f0d341c4eb9707cc37
Aniket Munim <aniketmunim@anikets-air.mynetworksettings.com>
2022-12-23 11:20:00 -0500

    initial commit

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express();
 const PORT = 5000
 
+//Middleware will execute and make the necessary changes
+//before executing get request.
+const customMiddleware = (res, req, next) => {
+    console.log("middleware executed!");
+    next();
+}
+
+app.use(customMiddleware);
+
 app.get('/', (req, res) => {
+    console.log('Home')
     res.send('Hello World')
+});
+
+app.get('/about', (req, res) => {
+    console.log('About')
+    res.send('About us')
 });
 
 app.listen(PORT, () => {
